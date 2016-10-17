f9dd23ee807fe16d4e7e5ccc4b83bc42d3ba119e
Alex Aguinaldo <psionicbones@gmail.com>
2016-10-17 08:46:51 -1000

    update

---
+++ javascript.js
@@ -0,0 +1,33 @@
+// Ref: http://slides.com/ifang/javascriptbasics#/0/1
+
+// this is a comment
+/* this is a block comment */
+
+// this is a string
+var myName = "Alex";
+
+// this is an integer
+var myAge = 17;
+
+// boolean type
+var booleanExample = true;
+
+//
+undefined
+
+// not a number
+NaN
+
+// array
+
+// objects
+
+var camelCaseExample 
+
+// example reminder: spaces count as characters and follows pemdas
+// var firstName = "Alex";
+// var lastName = "Aguinaldo";
+
+// console.log(firstName);
+
+// double equal (==) is used to compare
