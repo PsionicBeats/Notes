92dd6c017f4657805bcc13ec5a24174aa2db15bf
Alex Aguinaldo <psionicbones@gmail.com>
2016-10-26 08:15:34 -1000

    Update

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 // Ref: http://slides.com/ifang/javascriptbasics#/0/1
-
+//Run on HTTPServer
 // this is a comment
 /* this is a block comment */
 
@@ -19,10 +19,14 @@ undefined
 NaN
 
 // array
+var myArray = ['one', 2, 'seven', 'batman', true]
 
 // objects
 
-var camelCaseExample 
+// Null
+// Is written literally. 
+var HALP = null; HALP
+
 
 // example reminder: spaces count as characters and follows pemdas
 // var firstName = "Alex";
@@ -30,4 +34,35 @@ var camelCaseExample
 
 // console.log(firstName);
 
-// double equal (==) is used to compare
+// double equal AND (==) is used to compare
+
+// || OR
+
+// Funct is an execute statement
+function (){
+
+}
+
+function ohai (){
+	console.log ("hello i am a new function");
+}
+
+//invoking a function
+ohai ();
+
+// Can put parameters
+function hello (a, b) {
+	console.log(a + b);
+}
+
+hello(10, 11);
+
+function creategreeting(name, age) {
+	if ( age < 32){
+		console.log('hello ' + name);
+		}else{
+			console.log('hello Mr. ' + name);
+			return 'hello Mr. ' + name;
+			return ''
+		}
+}
