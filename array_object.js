048c50465d55b0d9377447c168d67466659120d3
Alex Aguinaldo <psionicbones@gmail.com>
2016-11-21 08:42:58 -1000

    update

---
+++ array_object.js
@@ -0,0 +1,11 @@
+var tan = ["john", "jon", "cool", 3, 11, 7, 9, null, undefined, ["no shirt", 27, null]]
+	console.log(tan[9][1]);
+
+var yoloSwag = {
+	Isaiah : "beast",
+	hair : "sunshine",
+	sayings : ["wait what?", "hold me i'm cold", "yoloha"],
+	position : "kicker"
+};
+
+console.log(yoloSwag.sayings[2]);
