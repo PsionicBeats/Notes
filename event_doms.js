2869d2e02c2f017a2098216907b93d3a9c17ea12
Alex Aguinaldo <psionicbones@gmail.com>
2016-12-12 08:45:40 -1000

    update

---
+++ event_doms.js
@@ -0,0 +1,16 @@
+/* What is the DOM? */
+/* The dom is an acronym for the Document Object Model, allowing the user to access and update a document through its outlook and contents. */
+
+/* What is a Javascript Event? */
+/* A Javascript Event is something that the browser or user can interact with. */
+
+/* Name and describe two types of Javascript Events */
+/* 	1. onclick - User clicks element
+		2. onmouseover - User scrolls over element
+
+/* How do you add a js externally to your index.html page? */
+<script type="text/javascript" src="pathway">
+
+/* How do you change a class or id  name in Javascript? */
+
+/* How do you change the bcakground color of a div element using Javascript? */
