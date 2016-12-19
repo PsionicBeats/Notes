5031dfff1cf512a6c85abebce28e9dab598c17f6
Alex Aguinaldo <psionicbones@gmail.com>
2016-12-19 00:44:17 -1000

    update

---
+++ js_string_methods.js
@@ -0,0 +1,16 @@
+/*List three string methods and explain what they do.*/
+String methods allow the user to work with strings, a few of which are indexOf(), search(), and substr().
+	indexOf() returns the whereabouts of the first use of certain text within a string.
+	search() is a method that searches a string for a value and returns with the positive match.
+	While substr() is similar to slice(), the difference is that the second parameter will state the length of the extracted portion. 
+/*Explain the following Math methods*/
+
+/*Math.round()*/
+- Returns "x" rounded to the nearest integer.
+/*Math.floor()*/
+- Math.floor is the opposite of Math.round, returning "x" rounded down to its nearest integer. 
+/*Math.Min() & Math.Max()*/
+- Using these Math methods will allow the user to find the lowest and highest value in a list of assorted arguments.
+
+/*Math.random()*/
+- Hence the word "random," the method will return a random number between an inclusive number such as zero and an exclusive number like one.
